ff55c3495b81ca0f9d451cf0b2746379dbc70048
Wisut Hantanong <wizzup@gmail.com>
2023-10-19 19:08:27 +0700

    pass 1st stage

--- app/Main.hs
+++ app/Main.hs
@@ -11,10 +11,10 @@ main = do
     BLC.putStrLn "Logs from your program will appear here"
 
     -- Uncomment this block to pass first stage
-    -- let host = "127.0.0.1"
-    --     port = "4221"
-    --
-    -- BLC.putStrLn $ "Listening on " <> BLC.pack host <> ":" <> BLC.pack port
-    --
-    -- serve (Host host) port $ \(serverSocket, serverAddr) ->
-    --     BLC.putStrLn $ "Accepted connection from " <> BLC.pack (show serverAddr) <> "."
+    let host = "127.0.0.1"
+        port = "4221"
+    
+    BLC.putStrLn $ "Listening on " <> BLC.pack host <> ":" <> BLC.pack port
+    
+    serve (Host host) port $ \(serverSocket, serverAddr) ->
+         BLC.putStrLn $ "Accepted connection from " <> BLC.pack (show serverAddr) <> "."
