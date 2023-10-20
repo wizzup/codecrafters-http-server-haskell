684e8f564b5e1188937281b934e29bed7196d384
Wisut Hantanong <wizzup@gmail.com>
2023-10-20 21:14:42 +0700

    stage 4

--- app/Main.hs
+++ app/Main.hs
@@ -7,27 +7,32 @@ import Network.Simple.TCP
 
 main :: IO ()
 main = do
-    -- You can use print statements as follows for debugging, they'll be visible when running tests.
-    BS.putStrLn "Logs from your program will appear here"
-
-    -- Uncomment this block to pass first stage
     let host = "127.0.0.1"
         port = "4221"
     
     BS.putStrLn $ "Listening on " <> BS.pack host <> ":" <> BS.pack port
     
     serve (Host host) port $ \(serverSocket, serverAddr) -> do
-         BS.putStrLn $ "Accepted connection from " <> BS.pack (show serverAddr) <> "."
+        BS.putStrLn $ "Accepted connection from " <> BS.pack (show serverAddr) <> "."
 
-         rcv <- fmap (BS.take 6 . head . BS.lines) <$> recv serverSocket 4096
-         case rcv of
-            Just "GET / " -> do
-                BS.putStrLn "OK"
-                send serverSocket "HTTP/1.1 200 OK\r\n\r\n"
-            Just _  -> do
-                BS.putStrLn "Not Found"
-                send serverSocket "HTTP/1.1 404 Not Found\r\n\r\n"
-            Nothing -> BS.putStrLn "Nothing"
+        rcv <- fmap (BS.words . head . BS.lines) <$> recv serverSocket 4096
+        print rcv
+        case rcv of
+            Just ("GET":"/":_) -> do
+              BS.putStrLn "Root"
+              send serverSocket "HTTP/1.1 200 OK\r\n\r\n"
 
+            Just ("GET":p:_) -> do
+                case BS.stripPrefix (BS.pack "/echo/") p of
+                    Just x -> do
+                        BS.putStrLn $ "Echo: " <> x
+                        send serverSocket 
+                            $ "HTTP/1.1 200 OK\r\n"
+                            <> "Content-Type: text/plain\r\n"
+                            <> "Content-Length: " <> (BS.pack . show $ BS.length x) <> "\r\n\r\n"
+                            <> x <> "\r\n"
+                    Nothing -> do
+                        BS.putStrLn "Non-echo"
+                        send serverSocket "HTTP/1.1 404 Not Found\r\n\r\n"
 
-        
+            _ -> BS.putStrLn "Ignored Nothing"
